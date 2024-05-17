47bf385fe757f2b41c17002e478e60b484b9a0fa
SodaBabajani <147845717+sodababajani@users.noreply.github.com>
2024-05-17 19:10:33 +0330

    actions

--- script/main.js
+++ script/main.js
@@ -9,5 +9,25 @@ restart.addEventListener('click',()=>{
 	})
 })
 
+const game = () =>{
+    let ai_score = 0;
+    let youer_score = 0;
+    const rock = document.querySelector('.rock');
+    const paper = document.querySelector('.paper');
+    const scissor = document.querySelector('.scissor');
+    const user_action = [rock, paper, scissor];
+    const computer_action = ['rock', 'paper', 'scissors'];
+    const playGame = () =>{
+        user_action.forEach(action => {
+            action.addEventListener('click', () => {
+                const actionList = action.classList;
+                const actionClassName = actionList[0];
+                const num = Math.floor(Math.random()*3);
+                const computer_choice = computer_action[num];
+                winner(actionClassName, computer_choice)
+            })
+        })
+    }
+
 
 
